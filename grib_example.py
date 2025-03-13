5c2b7574b8056721c64c58908104303dd6ffac29
rwaga10 <r.a.waga@student.tudelft.nl>
2025-03-13 10:16:03 +0100

    example for grib

---
+++ grib_example.py
@@ -0,0 +1,20 @@
+import xarray as xr
+import matplotlib.pyplot as plt
+import numpy as np
+
+'''
+The data is stored as numpy arrays, so all numpy functions can be used on it.
+'''
+
+# Open the GRIB file
+ds = xr.open_dataset("testdata2.grib", engine="cfgrib")
+
+var_points = ds.variables['w'][:].data  # all values of 't' in the dataset
+var_points_filtered = ds.variables['w'][:].data[0, 0, 0]  # [a, b, c] are indices of a: time, b: latitude, c: longitude
+
+time = ds.variables['valid_time'][:].data  # [s]
+time = (time - time[0]) / 3600  # normalise to t=0 at the first point, convert to hrs
+
+print(var_points_filtered)
+df = ds['w'].to_dataframe()
+print(df)
