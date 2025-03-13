a748d2af0fef71fef3e62929bfaf0406d4b8abc5
dominik <dominik.cienkowski@gmail.com>
2025-03-13 11:12:02 +0100

    plot of temperature vs time

--- GRIB_data.py
+++ GRIB_data.py
@@ -1,20 +1,35 @@
 import xarray as xr
+import pandas as pd
 import matplotlib.pyplot as plt
 
 # Open the GRIB file
-ds = xr.open_dataset("testdata1.grib", engine="cfgrib")
+ds = xr.open_dataset("testdata3.grib", engine="cfgrib")
 
-# Print dataset information
-#print(ds)
 
 # Access a variable
-temperature = ds['t']  # Example: 2-meter temperature
-#print(temperature)
-
-latitude = ds.variables['latitude'][:]
-#print(latitude)
-
+time = ds['time'].values # time
+temperature = ds['t2m'].values  # 2-meter temperature
+avg_temperature = temperature.mean(axis=(1, 2))
 
 # Convert to pandas DataFrame
-df = temperature.to_dataframe()
-print(df)
+#df_temp = temperature.to_dataframe()
+time_series = pd.to_datetime(time)
+time_hours = time_series.hour
+#print(df_temp)
+
+#print(time_hours)
+#print(avg_temperature)
+
+
+# Plot average temperature vs. time
+plt.figure(figsize=(8, 5))
+plt.plot(time_hours, avg_temperature, marker='o', linestyle='-', color='b', alpha=0.7)
+
+# Labels and title
+plt.xlabel("Hour of the Day")
+plt.ylabel("Average Temperature (°C)")
+plt.title("Average Temperature Variation Over Time")
+plt.grid(True)
+
+# Show plot
+plt.show()
