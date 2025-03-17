9554d69210c408bceebe3e9dc115cd9588e9380a
dominik <dominik.cienkowski@gmail.com>
2025-03-17 12:22:53 +0100

    potentail temp func 4

--- functions.py
+++ functions.py
@@ -1,8 +1,5 @@
 import xarray as xr
-import pandas as pd
-import math
-import numpy as np
-import matplotlib.pyplot as plt
+
 
 ds = xr.open_dataset("gemini-testdata.grib", engine="cfgrib")
 # print(ds.variables['q'])
