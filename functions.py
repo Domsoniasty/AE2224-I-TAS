5c260dcaada51fa5c5d137748a53842772f358d9
dominik <dominik.cienkowski@gmail.com>
2025-03-17 11:46:44 +0100

    potentail temp func 3

--- functions.py
+++ functions.py
@@ -5,16 +5,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ds = xr.open_dataset("gemini-testdata.grib", engine="cfgrib")
+# print(ds.variables['q'])
 
 
 def get_potential_temp(dataset, time, lat, long):
     temperature = dataset["t"].isel(time=time, latitude=lat, longitude=long).values
     pressure = dataset["isobaricInhPa"].values
-    temperature_pressure_pairs = list(zip(pressure, temperature))
+    specific_humidity = dataset["q"].isel(time=time, latitude=lat, longitude=long).values
+    temperature_pressure_pairs = list(zip(pressure, temperature, specific_humidity))
     pot_temp = []
-    for p, t in temperature_pressure_pairs:
+    for p, t, q in temperature_pressure_pairs:
         p_t = (t * (1000/p) ** 0.286)
-        pot_temp.append(p_t)
+        v_p_t = p_t * (1 + 0.61 * q)
+        pot_temp.append(v_p_t)
     return pot_temp
 
 
