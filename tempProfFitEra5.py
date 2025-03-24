7ac8b2177ff661cd0aa2c449ed99275beadd6415
rwaga10 <r.a.waga@student.tudelft.nl>
2025-03-24 08:53:07 +0100

    Udhaya's scripts

---
+++ tempProfFitEra5.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+# -*-
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+import tempProfFitFunctions as my
+
+
+#######################################################
+# Inputs
+#######################################################
+
+# Input NetCDF file
+inp_file = '/Users/udhaya_chandiran/Downloads/data_stream-oper.nc'
+
+# Inputs
+lat = 36.5   # latitude in deg.
+long = -97.5   # longitude in deg.
+hMax = 3000   # Maximum height of data used for profile fitting
+# Start and stop time of data used for profile fitting
+timeStart = np.datetime64('2023-08-23T10:00:00.000000000')
+timeEnd = np.datetime64('2023-08-23T15:00:00.000000000')
+
+# Step sizes in finding the optimum values
+del_l=1   # Step size in m for inversion height
+del2_h=1   # Step size in m for inversion thickness
+
+
+
+#######################################################
+# Outputs
+#######################################################
+
+# Full names of the output variables and their units
+# time: Array of time of the profiles
+# z: Array of heights (m)
+# theta_v: Time- and height- series of virtual potential temperatures (K)
+# invH: Time series of inversion heights (m)
+# invThic: Time series of inversion thickness (m)
+# blTemp: Time series of boundary layer temperature (K)
+# invStren: Time series of inversion strength (K)
+# gamma: Time series of free atmospheric lapse rate (K/m)
+# sumSqDifArr: Time series of the sum of the squared difference (K^2)
+
+
+
+# Find the profile fit
+time, z, theta_v, invH, invThic, blTemp, invStren, gamma, sumSqDifArr = \
+    my.findProfParam(inp_file, lat, long, hMax, del_l, del2_h, tStart=timeStart, tEnd=timeEnd)
+
+
+
+#######################################################
+# Plots
+#######################################################
+
+# Cut off value to identify good fits
+cutOff_S = 2
+
+# Plot the time series of all the parameters
+fig, ax = plt.subplots(6, 1, figsize=(8,10), dpi=300, sharex=True)
+
+ax[0].plot(time, invH)
+ax[1].plot(time, invThic)
+ax[2].plot(time, invStren)
+ax[3].plot(time, gamma*1000)
+ax[4].plot(time, blTemp)
+ax[5].plot(time, sumSqDifArr)
+ax[5].plot(time, cutOff_S*np.ones(time.shape), linestyle=":")
+
+ax[5].set_xlabel('$time$')
+ax[0].set_ylabel('$H_i \ (m)$')
+ax[1].set_ylabel('$\Delta H \ (m)$')
+ax[2].set_ylabel('$\Delta \Theta \ (K)$')
+ax[3].set_ylabel('$\Gamma \ (K/km)$')
+ax[4].set_ylabel('$\Theta _M \ (K)$')
+ax[5].set_ylabel('$S$')
+
+ax[5].tick_params(axis='x', labelrotation=90)
+
+
+
+#%%
+
+# Plot the actual ERA5 profile and the model profile
+# for either one time stamp (first line below)
+# or all time stamps (second line below)
+tPlotInd = 0
+# for tPlotInd in range(len(time)):
+
+# Step size in z for model profile plotting
+del_zPlot = 1
+
+# Compute the model profile
+zPlot = np.arange(0, hMax+del_zPlot, del_zPlot)
+eta = my.calc_eta(invH[tPlotInd], invThic[tPlotInd], zPlot)
+xPlot = [blTemp[tPlotInd], invStren[tPlotInd], gamma[tPlotInd]*invThic[tPlotInd]/3]
+theta_eta = my.calc_theta_eta(xPlot, eta)
+
+plt.plot(theta_v[tPlotInd, :], z, label='ERA5')
+plt.plot(theta_eta, zPlot, label='Fit')
+
+# The following plots can be helpful in better understanding the model
+# To see individual parameters from the model in the plot
+# plt.plot(theta_eta, invH[tPlotInd]*np.ones(theta_eta.shape), label='$H_i$', linestyle="--")
+# plt.plot(theta_eta, (invH[tPlotInd] - invThic[tPlotInd]/2)*np.ones(theta_eta.shape), label='$H_{i,Bottom}$', linestyle="--")
+# plt.plot(theta_eta, (invH[tPlotInd] + invThic[tPlotInd]/2)*np.ones(theta_eta.shape), label='$H_{i,Top}$', linestyle="--")
+# plt.plot(blTemp[tPlotInd] + gamma[tPlotInd]*zPlot, zPlot, label='$\Gamma$', linestyle="--")
+# plt.plot(blTemp[tPlotInd]*np.ones(zPlot.shape), zPlot, label='$\Theta _M$', linestyle="--")
+
+# To compare the different methods of finding the inversion top theta_v as discussed in the paper
+# plt.plot((blTemp[tPlotInd] + invStren[tPlotInd])*np.ones(zPlot.shape), zPlot, label='$\Theta _{i,Top,a}$', linestyle="--")
+# plt.plot((blTemp[tPlotInd] + invStren[tPlotInd] + gamma[tPlotInd]*invThic[tPlotInd]/3)*np.ones(zPlot.shape), zPlot, label='$\Theta _{i,Top,a+b}$', linestyle="--")
+# plt.plot((blTemp[tPlotInd] + invStren[tPlotInd] - gamma[tPlotInd]*invThic[tPlotInd]/3)*np.ones(zPlot.shape), zPlot, label='$\Theta _{i,Top,a-b}$', linestyle="--")
+
+plt.xlabel('$\Theta_v \ (K)$')
+plt.ylabel('$h \ (m)$')
+plt.legend()
+
+# plt.savefig(f'plots/tempProfFit{str(tPlotInd).zfill(3)}.jpeg', bbox_inches='tight')
+# plt.clf()
+
+
+#%%
---
+++ tempProfFitFunctions.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python3
+# -*-
+
+import numpy as np
+import xarray as xr
+
+
+
+def extDataFromNetCDF(inp_file, lat, long, hMax, tStart, tEnd):
+    
+    # Open the NetCDF file
+    ds = xr.open_dataset(inp_file)
+
+    # Dimensions: (valid_time: 48, pressure_level: 37, latitude: 13, longitude: 13)
+    # Coordinates and variables in the dataset
+    # valid_time      (valid_time) datetime64[ns] 384B 2023-08-23 ... 2023-08-2...
+    # pressure_level  (pressure_level) float64 296B 1e+03 975.0 950.0 ... 2.0 1.0
+    # latitude        (latitude) float64 104B 38.0 37.75 37.5 ... 35.5 35.25 35.0
+    # longitude       (longitude) float64 104B -99.0 -98.75 -98.5 ... -96.25 -96.0
+    # d: Divergence
+    # cc: Fraction of cloud cover
+    # z: Geopotential
+    # o3: Ozone mass mixing ratio
+    # pv: Potential vorticity
+    # r: Relative humidity
+    # ciwc: Specific cloud ice water content
+    # clwc: Specific cloud liquid water content
+    # q: Specific humidity
+    # crwc: Specific rain water content
+    # cswc: Specific snow water content
+    # t: Temperature
+    # u, v, and w: Components of velocity
+    # vo: Vorticity
+
+    # Convert vertical coordinates from pressure to altitude
+    # Formula was obtained from the below webpage
+    # https://www.weather.gov/epz/wxcalc_pressurealtitude
+    h = ( 1 - (ds.pressure_level.values/1013.25)**0.190284 ) * 145366.45 * 0.3048
+    hMaxInd = np.count_nonzero(h <= hMax)
+    z = h[:hMaxInd]
+
+    # Indices of latitude and longitude of interest
+    lat_i = np.argmin(abs(ds.latitude.values - lat))
+    long_i = np.argmin(abs(ds.longitude.values - long))
+
+    # Sanity Check
+    print(f"Maximum height: {h[hMaxInd-1]} m")
+    print(f"Chosen latitude: {ds.latitude[lat_i].values}")
+    print(f"Chosen longitude: {ds.longitude[long_i].values}")
+
+    # Extract time array
+    # time = ds.valid_time.values
+    if tStart == 'Not provided':
+        print('\nStart time is not provided!')
+        print('Starting from the first time stamp in the dataset!')
+        tStartInd = 0
+    else:
+        tStartInd = np.count_nonzero(ds.valid_time.values < tStart)
+
+    if tEnd == 'Not provided':
+        print('\nEnd time is not provided!')
+        print('Ending with the last time stamp in the dataset!')
+        tEndInd = len(ds.valid_time)
+    else:    
+        tEndInd = np.count_nonzero(ds.valid_time.values <= tEnd)
+    
+    time = ds.valid_time.values[tStartInd:tEndInd]
+
+    # Extract other needed variables
+    ds_t = ds.t[:, :, lat_i, long_i].values
+    ds_r = ds.r[:, :, lat_i, long_i].values
+    ds_q = ds.q[:, :, lat_i, long_i].values
+    ds_clwc = ds.clwc[:, :, lat_i, long_i].values
+    ds_ciwc = ds.ciwc[:, :, lat_i, long_i].values
+    ds_crwc = ds.crwc[:, :, lat_i, long_i].values
+    ds_cswc = ds.cswc[:, :, lat_i, long_i].values
+
+    # Convert temperature to potential temperature
+    theta = ds_t * (10*100/ds.pressure_level).values**0.286
+
+    # Convert potential temperature to virtual potential temperature
+    sat_sts = ds_r/100   # Saturation status
+    np.minimum(sat_sts, 1.0, out=sat_sts)   # 1 corresponds to saturated air
+    moi_dry_ratio = 1/(1 - ds_q - ds_clwc - ds_ciwc - ds_crwc - ds_cswc) # Moist air to dry air ratio
+    r_sat = moi_dry_ratio * (ds_q - sat_sts * (ds_r/100 - 1) * ds_q)
+    r_L = moi_dry_ratio * (ds_clwc + ds_crwc + sat_sts * (ds_r/100 - 1) * ds_q) 
+    theta_v = theta * (1 + 0.61*r_sat - r_L)
+    theta_v = theta_v[tStartInd:tEndInd, :hMaxInd]
+
+    # Return potential temperature, height and time
+    return theta_v, z, time
+
+
+
+def calc_eta(l, del_h, z):
+    # Constants
+    c = 1/3
+    eta = (z - l) / (c*del_h)
+    return eta
+
+
+
+def calc_f_eta(eta):
+    f_eta = (np.tanh(eta) + 1) / 2
+    return f_eta
+
+
+
+def calc_g_eta(eta):
+    g_eta = ( np.log( 2*np.cosh(eta) ) + eta ) / 2
+    return g_eta
+
+
+
+def calc_theta_eta(x, eta):
+    theta_eta = x[0] + x[1]*calc_f_eta(eta) + x[2]*calc_g_eta(eta)
+    return theta_eta
+
+
+
+def solveForCoeff(z, theta_i, l, del_h):
+
+    # Number of data points
+    N = len(theta_i)
+
+    # Non-dimensional height parameter
+    eta = calc_eta(l, del_h, z)
+
+    # Functions used in the model
+    f_eta = calc_f_eta(eta)
+    g_eta = calc_g_eta(eta)
+
+    # Forming the system of linear algebraic equations (A6)
+    # given in Rampanelli and Zardi 2004
+    # Ax = b
+    # where x = [theta_m   a   b]^T
+    A = np.array([[N, np.sum(f_eta), np.sum(g_eta)], \
+        [np.sum(f_eta), np.sum(f_eta**2), np.sum(g_eta*f_eta)], \
+            [np.sum(g_eta), np.sum(f_eta*g_eta), np.sum(g_eta**2)]])
+
+    b = np.array([np.sum(theta_i), np.sum(theta_i*f_eta), np.sum(theta_i*g_eta)])
+
+    # Solve the system of equation and return the results
+    x = np.linalg.solve(A, b)
+
+    # Calculate the variance between the measurements and the model
+    theta_eta = calc_theta_eta(x, eta)
+    sumSqDif = np.sum((theta_eta - theta_i)**2)
+    
+    return sumSqDif, x
+
+
+
+def findProfParam(inp_file, lat, long, hMax, del_l, del2_h, tStart='Not provided', tEnd='Not provided'):
+
+    # Extract height, time and temperature and compute virtual potential temperature
+    theta_v, z, time = extDataFromNetCDF(inp_file, lat, long, hMax, tStart, tEnd)
+
+    # Initialize arrays
+    invH = np.zeros(len(time))   # Inversion Height
+    invThic = np.zeros(len(time))   # Inversion Thickness
+    invStren = np.zeros(len(time))   # Inversion Strength
+    blTemp = np.zeros(len(time))   # Boundary Layer Temperature
+    gamma = np.zeros(len(time))
+    sumSqDifArr = np.zeros(len(time))   # Sum of squared difference
+
+    for tInd in range(len(time)): 
+
+        # Virtual potential temperature
+        theta_i = theta_v[tInd]
+
+        # To have a value for comparison in the loop
+        # The end points that the loops will ignore are calculated here
+        lCurr = z[-1]
+        del_hCurr = np.max( z[1:]-z[:-1] )
+        sumSqDifBest, xBest = solveForCoeff(z, theta_i, lCurr, del_hCurr)
+        lBest = lCurr
+        del_hBest = del_hCurr
+
+        # Compute the model residual for each l and del_h
+        for lCurr in np.arange(z[0], z[-1], del_l):
+            for del_hCurr in np.arange(np.min(z[1:]-z[:-1]), np.max(z[1:]-z[:-1]), del2_h):
+                sumSqDifCurr, xCurr = solveForCoeff(z, theta_i, lCurr, del_hCurr)
+                # Check if this model is better
+                if sumSqDifCurr < sumSqDifBest:
+                    sumSqDifBest = sumSqDifCurr
+                    xBest = xCurr
+                    lBest = lCurr
+                    del_hBest = del_hCurr
+
+        # Save the best model parameters
+        invH[tInd] = lBest
+        invThic[tInd] = del_hBest
+        blTemp[tInd] = xBest[0]
+        invStren[tInd] = xBest[1]   # a+b gives the potential temperature jump in the Entrainment Layer,
+                                    # However the paper suggests to use 'a' for inversion strength
+        gamma[tInd] = 3 * xBest[2] / del_hBest   # The paper says 2*b/del_h, but it seems like a typo
+        sumSqDifArr[tInd] = sumSqDifBest
+
+
+    return time, z, theta_v, invH, invThic, blTemp, invStren, gamma, sumSqDifArr
