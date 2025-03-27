6041adbd3ee89f288cbe2146328aa30f7baf7560
Alejandro Calatayud <a.calatayudpisarevsky@student.tudelft.nl>
2025-03-27 09:25:20 +0100

    I believe these are the wind headings in degrees

--- Wind direction.py
+++ Wind direction.py
@@ -5,5 +5,6 @@ ds = xr.open_dataset("54.2_5_2020-07-11T01(00(00.00_2020-07-11T03(00(00.00.nc")
 print (ds)
 u_component = ds['u'].values
 v_component = ds['v'].values
-direction = (np.arctan(v_component/u_component)/np.pi)*180
-print (direction)
+time = ds['t'].values
+direction_vector = (v_component/u_component)/(np.sqrt(u_component**2 + v_component**2))
+print("direction_vector")
