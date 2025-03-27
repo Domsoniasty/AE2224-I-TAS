e66c5282e3b86d985f3f8efcbe0f309701ac0cb1
Alejandro Calatayud <a.calatayudpisarevsky@student.tudelft.nl>
2025-03-27 10:15:48 +0100

    I believe these are the wind headings in degrees

--- Wind direction.py
+++ Wind direction.py
@@ -1,10 +1,20 @@
 import xarray as xr
 import numpy as np
+import math
 
-ds = xr.open_dataset("54.2_5_2020-07-11T01(00(00.00_2020-07-11T03(00(00.00.nc")
-print (ds)
-u_component = ds['u'].values
-v_component = ds['v'].values
-time = ds['t'].values
-direction_vector = (v_component/u_component)/(np.sqrt(u_component**2 + v_component**2))
-print("direction_vector")
+
+def get_direction(dataset):
+    u_components = dataset.variables['u'].data[0, :, 0, 0]
+    v_components = dataset.variables['v'].data[0, :, 0, 0]
+
+    # direction unit vector
+    direction = np.arctan2(u_components, v_components)/np.sqrt(u_components**2 + v_components**2)
+    return direction
+
+
+# Example usage
+dataset = xr.open_dataset("54.2_5_2020-07-11T01(00(00.00_2020-07-11T03(00(00.00.nc")
+direction_vector = get_direction(dataset)
+print(direction_vector)
+
+
