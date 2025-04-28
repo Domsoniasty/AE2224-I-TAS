71ff6329fadd2d28f5ac88e4f54e068b316160bb
dominik <dominik.cienkowski@gmail.com>
2025-04-28 09:51:22 +0200

    bit cleaning

--- main.py
+++ main.py
@@ -6,37 +6,30 @@ import functions as fnc
 
 
 # Create a list of datafiles' names
-def var_arrays(tInd=1, direction = -1, pressLvl=975):
+def var_arrays(tInd=1, direction=-1, pressLvl=975):
 
-    '''
-    :param tInd: index of the time stamp to use; 1 is the middle (closest to gravity waves occurrence)
-    :param direction: -1 is upstream, 1 is downstream, 0 is at the farm
-    :return: array of the following arrays (in order): bool of whether gravity waves occur, virtual pot. temp, inversion height, inversion thickness, inversion strength,
-    free atm. lapse rate, horizontal windspeed, vertical windspeed
-    '''
+    # :param tInd: index of the time stamp to use; 1 is the middle (closest to gravity waves occurrence)
+    # :param direction: -1 is upstream, 1 is downstream, 0 is at the farm
+    # :return: array of the following arrays (in order): bool of whether gravity waves occur, virtual pot. temp,
+    # inversion height, inversion thickness, inversion strength,
+    # free atm. lapse rate, horizontal windspeed, vertical windspeed
 
-    downstream_dist = 0.25 # deg lat/long
+    downstream_dist = 0.25  # deg lat/long
     files = []
+
     # print(list(pathlib.Path('data').glob('*/*')))
     for path in list(pathlib.Path('data').glob('*/*')):
-        if  str(path)[-2:] == 'nc': # str(path)[0] != '.' and
+        if str(path)[-2:] == 'nc':  # str(path)[0] != '.' and
             files.append(str(path))
-    # print(files)
-    # print(list(pathlib.Path('data').glob('*/*')))
 
-    # theta_vArr = np.empty(len(files))
-    # invHArr = np.empty(len(files))
-    # invThicArr = np.empty(len(files))
-    # invStrenArr = np.empty(len(files))
-    # gammaArr = np.empty(len(files))
-    # rSqArr = np.empty(len(files))
     results = np.empty((len(files), 8))
     rSqMax = 0
-    count = 0 # count used for progress tracking of loading files
-    percentage = 0 #percentage for loading count
+    count = 0  # count used for progress tracking of loading files
+    percentage = 0  # percentage for loading count
     print_load = True
     print((len(files)))
-    for i in range(len(files)): # for each datafile
+    for i in range(len(files)):  # for each datafile
+
         # name processing
         case = files[i]
         print(case)
@@ -49,20 +42,27 @@ def var_arrays(tInd=1, direction = -1, pressLvl=975):
 
         caseSplit = case.split('\\')[-1].split('_') # extract info from name
         lat = float(caseSplit[0])
-        # print('lat:', lat)
         long = float(caseSplit[1])
         lat, long = np.array([lat, long])
-        try: caseSplit[2] = caseSplit[2].replace('.nc', '')
-        except: pass
-        # print(caseSplit[2])
-        if caseSplit[2] == 'Yes': gravity_waves = True
-        else: gravity_waves = False
-        #timeStart = np.datetime64(timeStart.replace('(', ':')) # colons can't be used in python filenames
-        #timeEnd = np.datetime64(timeEnd.replace('(', ':').replace('.nc', '')) # also remove the extension '.nc'
+
+        try:
+            caseSplit[2] = caseSplit[2].replace('.nc', '')
+        except:
+            pass
+
+        if caseSplit[2] == 'Yes':
+            gravity_waves = True
+        else:
+            gravity_waves = False
+
+        # timeStart = np.datetime64(timeStart.replace('(', ':')) # colons can't be used in python filenames
+        # timeEnd = np.datetime64(timeEnd.replace('(', ':').replace('.nc', '')) # also remove the extension '.nc'
 
         # read in the data
         ds = xr.open_dataset(case)
-        _, _, theta_vArr, invH, invThic, _, invStren, gamma, _, invrSq = inv.inversion(ds, lat, long, tInd) # time, z, theta_v, invH, invThic, blTemp, invStren, gamma, sumSqDifArr
+
+        # time, z, theta_v, invH, invThic, blTemp, invStren, gamma, sumSqDifArr
+        _, _, theta_vArr, invH, invThic, _, invStren, gamma, _, invrSq = inv.inversion(ds, lat, long, tInd)
 
         theta_v = theta_vArr[0][int((1000-pressLvl)/25)]
         uArr = fnc.get_variable(case, 'u', pressLvl, lat, long)[tInd]
@@ -70,13 +70,12 @@ def var_arrays(tInd=1, direction = -1, pressLvl=975):
         horSpeed = np.sqrt(uArr**2 + vArr**2)
         verSpeed = fnc.get_variable(case, 'w', pressLvl, lat, long)[tInd]
         results[i] = np.array([gravity_waves, theta_v, invH[0], invThic[0], invStren[0], gamma[0], horSpeed, verSpeed])
-        if invrSq[0] > rSqMax: rSqMax = invrSq[0]
-        #if invrSq < 0.9: print('r2:', invrSq)
+
+        if invrSq[0] > rSqMax:
+            rSqMax = invrSq[0]
         print('r2:', invrSq)
     export = open('data_exported.npy', 'wb')
     np.save(export, results)
     export.close()
     return results
-# print(var_arrays()[0])
-# var_arrays()
 
